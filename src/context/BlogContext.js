93e944be74970599bfb8d5dcf3d3a1b305c9c5f4
Elliot Burin <eburin2@gmail.com>
2019-12-15 18:31:02 -0800

    edit functionality works as intended

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,10 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
@@ -25,10 +29,19 @@ const blogReducer = (state, action) => {
     }
   }
 
+  const editBlogPost = (dispatch) => {
+    return (id, title, content) => {
+      dispatch({ type: 'edit_blogpost', payload: { id, title, content }
+      });
+    }
+  }
+
   const deleteBlogPost = (dispatch) => {
     return (id) => {
       dispatch({ type: 'delete_blogpost', payload: id })
     }
   }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'Test', content: 'Test', id: 1}]);
+
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'Test', content: 'Test', id: 1}]);
